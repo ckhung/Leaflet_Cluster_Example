0ce2cc1d12e6677920ed37d76a6592082ffe7d4e
Chao-Kuei Hung <ckhung@cyut.edu.tw>
2022-06-03 12:34:10 +0800

    update Readme.md. now reads geojson. kh-bus-stops.geojson

--- Readme.md
+++ Readme.md
@@ -1,12 +1,26 @@
 Leaflet Cluster Example
 --
 
-Example of creating an interactive map with clustered data using Leaflet and the [Leaflet.markercluster](https://github.com/Leaflet/Leaflet.markercluster) plugin by [Dave Leaver](https://github.com/danzel).
+This is a leaflet map of clustered markers using Leaflet and the
+[Leaflet.markercluster](https://github.com/Leaflet/Leaflet.markercluster)
+plugin by [Dave Leaver](https://github.com/danzel).
 
-See the post for details: https://asmaloney.com/2015/06/code/clustering-markers-on-leaflet-maps
+You may have to put this directory somewhere your
+web server can display, such as /var/www/html
+or ~/public\_html (if userdir is enabled) for apache2.
+Or without any web server, you can open the index.html file as a local file
+in your browser provided that (A) you started the chrome
+browser with the --allow-file-access-from-files option or
+(B) security.fileuri.strict\_origin\_policy is set to False in your firefox.
+See [javascriptCanReadLocalFiles](https://github.com/ckhung/javascriptCanReadLocalFiles) for details.
 
-(The image icons came from here: [clker.com](https://www.clker.com/clipart-google-maps-pin-blue.html))
+To display your own data file, put your geojson file in the
+map/ directory and create a symbolic link "markers.geojson" to it.
+Then refresh your browser.
+All subfields in the "properties" field of each "feature"
+in the geojson file are displayed when the marker is clicked.
+Property names which begin with "name" are displayed first,
+other properties are sorted according to their names.
 
-10 June 2015  
-Andy Maloney  
-https://asmaloney.com
+This project was originally cloned from [Andy Maloney's Leaflet Cluster Example](https://github.com/asmaloney/Leaflet_Cluster_Example).
+See [his post](https://asmaloney.com/2015/06/code/clustering-markers-on-leaflet-maps) for details.
--- maps/leaf-demo.js
+++ maps/leaf-demo.js
@@ -1,51 +1,58 @@
 /* jshint esversion: 6 */
 // See post: http://asmaloney.com/2015/06/code/clustering-markers-on-leaflet-maps
 
-var map = L.map('map', {
-  center: [24.1944, 120.6625],
-  zoom: 11,
+$.getJSON('maps/markers.json', function(markers) {
+  main(markers);
 });
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  subdomains: ['a', 'b', 'c'],
-}).addTo(map);
-
-var myURL = jQuery('script[src$="leaf-demo.js"]')
-  .attr('src')
-  .replace('leaf-demo.js', '');
-
-var myIcon = L.icon({
-  iconUrl: myURL + 'images/pin24.png',
-  iconRetinaUrl: myURL + 'images/pin48.png',
-  iconSize: [29, 24],
-  iconAnchor: [9, 21],
-  popupAnchor: [0, -14],
-});
-
-var markerClusters = L.markerClusterGroup();
-
-for (var i = 0; i < markers.length; ++i) {
-  var name = '',
+function main(markers) {
+  var map = L.map('map', {
+    center: [23.611, 120.768],
+    zoom: 7,
+  });
+  
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: ['a', 'b', 'c'],
+  }).addTo(map);
+  
+  var myURL = jQuery('script[src$="leaf-demo.js"]')
+    .attr('src')
+    .replace('leaf-demo.js', '');
+  
+  var myIcon = L.icon({
+    iconUrl: myURL + 'images/pin24.png',
+    iconRetinaUrl: myURL + 'images/pin48.png',
+    iconSize: [29, 24],
+    iconAnchor: [9, 21],
+    popupAnchor: [0, -14],
+  });
+  
+  var markerClusters = L.markerClusterGroup();
+  
+  if ('features' in markers) markers = markers.features;
+  for (var i = 0; i < markers.length; ++i) {
+    var name = '',
       popup = '',
       prop = markers[i].properties;
-  var keys = Object.keys(prop);
-  for (const k of keys) {
+    var keys = Object.keys(prop);
+    for (const k of keys) {
       var tmp = k + ': ' + prop[k] + '<br />';
       if (k.match(/^\d*name\w*$/)) {
-	  name += tmp;
+        name += tmp;
       } else {
-	  popup += tmp;
+        popup += tmp;
       }
+    }
+  
+    var coord = markers[i].geometry.coordinates;
+    var m = L.marker([coord[1], coord[0]], {
+      icon: myIcon,
+    }).bindPopup(name+popup);
+  
+    markerClusters.addLayer(m);
   }
-
-  var coord = markers[i].geometry.coordinates;
-  var m = L.marker([coord[1], coord[0]], {
-    icon: myIcon,
-  }).bindPopup(name+popup);
-
-  markerClusters.addLayer(m);
+  map.addLayer(markerClusters);
 }
 
-map.addLayer(markerClusters);
